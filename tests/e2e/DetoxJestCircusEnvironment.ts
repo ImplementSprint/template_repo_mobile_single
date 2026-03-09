d5269c0ae1ba8227523a55e6f0c4611f129ce62d
tone <antoneeeeetorres@gmail.com>
2026-03-09 23:50:25 +0800

    chore: update jest-expo version and refactor DetoxJestCircusEnvironment
    - Updated jest-expo dependency from ^47.0.1 to ~54.0.13 in package.json.
    - Refactored DetoxJestCircusEnvironment to use detox's built-in test environment, removing custom implementation in both .cjs and .ts files.

--- tests/e2e/DetoxJestCircusEnvironment.ts
+++ tests/e2e/DetoxJestCircusEnvironment.ts
@@ -1,28 +1,5 @@
 // @ts-nocheck
 
-import jestEnvironmentNode from 'jest-environment-node';
-import CircusTestEventListeners from 'detox/runners/jest/CircusTestEventListeners.js';
+import DetoxTestEnvironment from 'detox/runners/jest/testEnvironment';
 
-const NodeEnvironment =
-  jestEnvironmentNode.TestEnvironment || jestEnvironmentNode.default || jestEnvironmentNode;
-
-class DetoxJestCircusEnvironment extends NodeEnvironment {
-  constructor(config, context) {
-    super(config, context);
-    this.testEventListeners = new CircusTestEventListeners();
-
-    Reflect.set(this.global, 'detoxCircus', {
-      getEnv: () => this,
-    });
-  }
-
-  addEventsListener(listener) {
-    this.testEventListeners.addListener(listener);
-  }
-
-  async handleTestEvent(event, state) {
-    await this.testEventListeners.notifyAll(event, state);
-  }
-}
-
-module.exports = DetoxJestCircusEnvironment;
+export = DetoxTestEnvironment;
